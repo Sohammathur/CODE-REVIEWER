943b71949ca997599328b960fdd59cfe31dd4551
Soham Mathur <147188771+smone30@users.noreply.github.com>
2025-08-09 21:50:18 +0530

    Initial commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,91 @@
+import { useState, useEffect } from 'react'
+import "prismjs/themes/prism-tomorrow.css"
+import Editor from "react-simple-code-editor"
+import prism from "prismjs"
+import Markdown from "react-markdown"
+import rehypeHighlight from "rehype-highlight";
+import "highlight.js/styles/github-dark.css";
+import axios from 'axios'
+import { SiC } from "react-icons/si"; 
+import './App.css'
+
+function App() {
+  const [code, setCode] = useState(`function sum() {
+  return 1 + 1
+}`)
+  const [review, setReview] = useState(``)
+  const [isReviewing, setIsReviewing] = useState(false)
+
+  useEffect(() => {
+    prism.highlightAll()
+  }, [])
+
+  async function reviewCode() {
+    setIsReviewing(true)
+    try {
+      const response = await axios.post('http://localhost:3000/ai/get-review', { code })
+      setReview(response.data)
+    } catch (error) {
+      setReview("❌ Error fetching review")
+    }
+    setIsReviewing(false)
+  }
+
+  return (
+    <>
+      <header style={{
+        display: "flex",
+        justifyContent: "space-between",
+        alignItems: "center",
+        padding: "0.75rem 1.5rem",
+        backgroundColor: "#0d2538", 
+        color: "white",
+        fontWeight: "bold",
+        fontSize: "1.2rem",
+        boxShadow: "0 2px 5px rgba(0,0,0,0.3)"
+      }}>
+        <div style={{ display: "flex", alignItems: "center", gap: "0.5rem" }}>
+          <SiC size={28} color="#A8B9CC" /> 
+          CODE-REVIEWER
+        </div>
+      </header>
+
+      <main>
+        <div className="left">
+          <div className="code">
+            <Editor
+              value={code}
+              onValueChange={code => setCode(code)}
+              highlight={code => prism.highlight(code, prism.languages.javascript, "javascript")}
+              padding={10}
+              style={{
+                fontFamily: '"Fira code", "Fira Mono", monospace',
+                fontSize: 16,
+                border: "1px solid #ddd",
+                borderRadius: "5px",
+                height: "100%",
+                width: "100%"
+              }}
+            />
+          </div>
+          <div
+            onClick={reviewCode}
+            className="review"
+            style={{
+              backgroundColor: isReviewing ? "#8888ff" : "rgb(186, 186, 235)"
+            }}
+          >
+            {isReviewing ? "Reviewing..." : "Review"}
+          </div>
+        </div>
+        <div className="right">
+          <Markdown rehypePlugins={[rehypeHighlight]}>
+            {review}
+          </Markdown>
+        </div>
+      </main>
+    </>
+  )
+}
+
+export default App
